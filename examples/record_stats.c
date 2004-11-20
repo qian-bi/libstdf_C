e5775119460f9ff67baef1a95adb7a8ad1facc38
Mike Frysinger <vapier@gmail.com>
2004-11-20 06:53:09 +0000

    support libhash and glib (but prefer libhash)
    Signed-off-by: Mike Frysinger <vapier@gmail.com>

--- examples/record_stats.c
+++ examples/record_stats.c
@@ -13,15 +13,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#if HAVE_GLIB
-#include <glib.h>
-#endif
 
-#if HAVE_GLIB
+#if HAVE_HASH_H
+#include <hash.h>
+#define HASH_VARS \
+	void *key, *val; \
+	hash hash_table; \
+	struct hash_iterator hit;
+#define HASH_INIT \
+	hash_initialise(&hash_table, 61, \
+	                hash_hash_string, hash_compare_string, hash_copy_string, \
+	                free, free);
+#define HASH_UPDATE \
+	key = (void*)recname; \
+	if (!hash_retrieve(&hash_table, key, (void**)&val)) { \
+		val = (void*)malloc(sizeof(long)); \
+		*(long*)val = 0; \
+		hash_insert(&hash_table, key, val); \
+	} \
+	(*(long*)val)++;
+#define HASH_PRINT \
+	key = NULL; \
+	hash_iterator_initialise(&hash_table, &hit); \
+	while (hash_fetch_next(&hash_table, &hit, (void **)&key, (void **)&val)) \
+		printf("\t%s : %li\n", (char*)key, *(long*)val); \
+	hash_iterator_deinitialise(&hash_table, &hit); \
+	hash_deinitialise(&hash_table);
+
+#elif HAVE_GLIB
+#include <glib.h>
+#define HASH_VARS \
+	GHashTable *hash_table; \
+	long *stat;
+#define HASH_INIT \
+	hash_table = g_hash_table_new(g_str_hash, g_str_equal);
+#define HASH_UPDATE \
+	stat = g_hash_table_lookup(hash_table, recname); \
+	if (!stat) { \
+		stat = (long*)malloc(sizeof(long)); \
+		*stat = 0; \
+		g_hash_table_insert(hash_table, g_strdup(recname), stat); \
+	} \
+	(*stat)++;
+#define HASH_PRINT \
+	g_hash_table_foreach(hash_table, print_stat, NULL); \
+	g_hash_table_destroy(hash_table);
 void print_stat(gpointer key, gpointer value, gpointer user_data)
 {
 	printf("\t%s : %li\n", (char*)key, *((long*)value));
 }
+
+#else
+#define HASH_VARS
+#define HASH_INIT
+#define HASH_UPDATE
+#define HASH_PRINT
 #endif
 
 int main(int argc, char *argv[])
@@ -29,12 +75,9 @@ int main(int argc, char *argv[])
 	stdf_file *f;
 	char *recname;
 	rec_unknown *rec;
-#if HAVE_GLIB
-	GHashTable *hash_table;
-	long *stat;
-#endif
 	long cnt;
 	int i;
+	HASH_VARS
 
 	if (argc <= 1) {
 		printf("Need some files to open!\n");
@@ -49,29 +92,17 @@ for (i=1; i<argc; ++i) {
 		continue;
 	}
 
-#if HAVE_GLIB
-	hash_table = g_hash_table_new(g_str_hash, g_str_equal);
-#endif
+	HASH_INIT
 	cnt = 0;
 	while ((rec=stdf_read_record(f)) != NULL) {
 		recname = stdf_get_rec_name(rec->header.REC_TYP, rec->header.REC_SUB);
-#if HAVE_GLIB
-		stat = g_hash_table_lookup(hash_table, recname);
-		if (!stat) {
-			stat = (long*)malloc(sizeof(long));
-			*stat = 0;
-			g_hash_table_insert(hash_table, g_strdup(recname), stat);
-		}
-		(*stat)++;
-#endif
+		HASH_UPDATE
 		cnt++;
 		stdf_free_record(rec);
 	}
 	stdf_close(f);
-#if HAVE_GLIB
-	g_hash_table_foreach(hash_table, print_stat, NULL);
-	g_hash_table_destroy(hash_table);
-#endif
+	HASH_PRINT
+
 	printf("\tTOTAL : %li\n", cnt);
 }
 	return EXIT_SUCCESS;
