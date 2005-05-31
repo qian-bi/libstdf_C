9266e74b16cd09f99655cfeaff194a828d90b90c
Mike Frysinger <vapier@gmail.com>
2005-05-31 23:08:22 +0000

    copyright update
    Signed-off-by: Mike Frysinger <vapier@gmail.com>

--- include/libstdf_types.h
+++ include/libstdf_types.h
@@ -3,7 +3,7 @@
  * @brief Public types.
  */
 /*
- * Copyright (C) 2004 Mike Frysinger <vapier@gmail.com>
+ * Copyright (C) 2004-2005 Mike Frysinger <vapier@gmail.com>
  * Released under the BSD license.  For more information,
  * please see: http://opensource.org/licenses/bsd-license.php
  *
