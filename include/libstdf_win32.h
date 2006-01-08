2dc1fb9526a823faa53acf99b7e3ef15b0bd52cd
Mike Frysinger <vapier@gmail.com>
2006-01-08 05:46:31 +0000

    pointless whitespace tweaks
    Signed-off-by: Mike Frysinger <vapier@gmail.com>

--- include/libstdf_win32.h
+++ include/libstdf_win32.h
@@ -3,7 +3,7 @@
  * @brief Win32 specific definitions.
  */
 /*
- * Copyright (C) 2004-2005 Mike Frysinger <vapier@gmail.com>
+ * Copyright (C) 2004-2006 Mike Frysinger <vapier@gmail.com>
  * Released under the BSD license.  For more information,
  * please see: http://opensource.org/licenses/bsd-license.php
  *
@@ -18,26 +18,26 @@
 #include <sys/types.h>
 
 #if !defined(__MINGW_H)
-typedef unsigned __int8		uint8_t;
-typedef unsigned __int16	uint16_t;
-typedef unsigned __int32	uint32_t;
-typedef unsigned __int64	uint64_t;
-typedef signed __int8		int8_t;
-typedef signed __int16		int16_t;
-typedef signed __int32		int32_t;
-typedef signed __int64		int64_t;
+typedef unsigned __int8    uint8_t;
+typedef unsigned __int16   uint16_t;
+typedef unsigned __int32   uint32_t;
+typedef unsigned __int64   uint64_t;
+typedef signed __int8      int8_t;
+typedef signed __int16     int16_t;
+typedef signed __int32     int32_t;
+typedef signed __int64     int64_t;
 #endif
 
 /* pos windows compiler doesnt like 'far' as a variable name */
-#define	far			_windows_is_a_pos_far
+#define far _windows_is_a_pos_far
 
 #include <io.h>
-#define	open	_open
+#define open _open
 
 #if !defined(BYTE_ORDER)
-#define	LITTLE_ENDIAN	1234
-#define	BIG_ENDIAN		4321
-#define	BYTE_ORDER		LITTLE_ENDIAN
+# define LITTLE_ENDIAN     1234
+# define BIG_ENDIAN        4321
+# define BYTE_ORDER        LITTLE_ENDIAN
 #endif
 
 #endif /* _LIBSTDF_WIN32_H */
--- include/libstdf_win32_options.h
+++ include/libstdf_win32_options.h
@@ -4,7 +4,7 @@
  * @internal
  */
 /*
- * Copyright (C) 2004-2005 Mike Frysinger <vapier@gmail.com>
+ * Copyright (C) 2004-2006 Mike Frysinger <vapier@gmail.com>
  * Released under the BSD license.  For more information,
  * please see: http://opensource.org/licenses/bsd-license.php
  *
@@ -14,21 +14,21 @@
 #ifndef _LIBSTDF_WIN32_OPTIONS_H
 #define _LIBSTDF_WIN32_OPTIONS_H
 
-#define	HAVE_ZIP	0
-#define	HAVE_GZIP	0
-#define	HAVE_BZIP2	0
-#define	HAVE_LZW	0
+#define HAVE_ZIP     0
+#define HAVE_GZIP    0
+#define HAVE_BZIP2   0
+#define HAVE_LZW     0
 
 /* you shouldn't have to touch anything below here */
-#define	HAVE_CTYPE_H		1
-#define	HAVE_ERRNO_H		1
-#define	HAVE_FCNTL_H		1
-#define	HAVE_STDARG_H		1
-#define	HAVE_STDIO_H		1
-#define	HAVE_STDLIB_H		1
-#define	HAVE_STRING_H		1
-#define	HAVE_SYS_STAT_H		1
-#define	HAVE_SYS_TYPES_H	1
-#define	HAVE_TIME_H			1
+#define HAVE_CTYPE_H       1
+#define HAVE_ERRNO_H       1
+#define HAVE_FCNTL_H       1
+#define HAVE_STDARG_H      1
+#define HAVE_STDIO_H       1
+#define HAVE_STDLIB_H      1
+#define HAVE_STRING_H      1
+#define HAVE_SYS_STAT_H    1
+#define HAVE_SYS_TYPES_H   1
+#define HAVE_TIME_H        1
 
 #endif /* _LIBSTDF_WIN32_OPTIONS_H */
