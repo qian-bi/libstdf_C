54a75fcc43cbf5dff8073d2b76fcca6be2dd8820
Mike Frysinger <vapier@gmail.com>
2006-01-08 05:47:48 +0000

    only copy .m4 files
    Signed-off-by: Mike Frysinger <vapier@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@ if test -d CVS ; then
 	if test -d ../m4 ; then
 		rm -rf m4 || exit 1
 		mkdir -p m4 || exit 1
-		cp ../m4/* m4/ || exit 1
+		cp ../m4/*.m4 m4/ || exit 1
 	fi
 	if test -d ../liblzw ; then
 		cp ../liblzw/lzw.h include/ || exit 1
