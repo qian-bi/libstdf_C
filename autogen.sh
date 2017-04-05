452e235499710951b66e05a71c9415d9ca3f5a0d
Stefan Brandner <stefan.brandner@gmx.at>
2017-04-05 15:13:28 +0200

    added lzw.h and lzw.c permanent

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -ex
 
-if [ -d .git ] ; then
-	if [ -d ../liblzw ] ; then
-		cp ../liblzw/lzw.h include/
-		cp ../liblzw/lzw.c src/
-	fi
-fi
-
 autoreconf -i -f
 
 # broken autotools wrongly clobbers our INSTALL
